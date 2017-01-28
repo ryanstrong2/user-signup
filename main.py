7ed166e420f58fe8a96e23afdde22b4e73118bd2
Ryan Strong <ryanstrong2@yahoo.com>
2017-01-28 13:08:10 -0600

    initial

--- main.py
+++ main.py
@@ -14,12 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# The form elements where the user inputs their username, password, password again, and email address must be named "username", "password", "verify", and "email", respectively.
+# The form method must be POST, not GET.
+# Upon invalid user input, your web app should re-render the form for the user.
+# Upon valid user input, your web app should redirect to a welcome page for the user.
+#
+#     This page must include both "Welcome" and the user's username.
+#
+
 import webapp2
+import cgi
+import re
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+    def post(self):
+        self.response.write('Welcome', username)
+    if not (username and password and verify):
+        self.form("Not a valid username.")
+    else:
+        self.redirect('/welcome')
 
+
+    Username: "^[a-zA-Z0-9_-]{3,20}$"
+    Password: "^.{3,20}$"
+    Email: "^[\S]+@[\S]+.[\S]+$"
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    retrun USER_RE.match(username)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
